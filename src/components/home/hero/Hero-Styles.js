a92312b43d88ca2a0f663a085020bdb57c735462
sreya-kata <sreya.kata@gmail.com>
2023-07-18 14:02:24 -0400

    add hand wave to hero

--- src/components/home/hero/Hero-Styles.js
+++ src/components/home/hero/Hero-Styles.js
@@ -1,9 +1,30 @@
 import styled from "styled-components";
-import { H3 } from "../../../constants/typography";
+import { H1, H3 } from "../../../constants/typography";
+import { devices } from "../../../constants/devices";
 
 const Banner = styled.section`
   margin-top: 3em;
   text-align: center;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleText = styled(H1)`
+  margin-bottom: 0;
+`;
+
+const TitleGif = styled.img`
+  width: 3rem;
+  margin-left: 0.75rem;
+
+  @media ${devices.laptop} {
+    width: 4rem;
+    margin-left: 1rem;
+  }
 `;
 
 const Subtitle = styled(H3)`
@@ -16,4 +37,4 @@ const HeroButtons = styled.div`
   margin-bottom: 2.2rem;
 `;
 
-export { Banner, Subtitle, HeroButtons };
+export { Banner, Subtitle, Title, TitleText, TitleGif, HeroButtons };
